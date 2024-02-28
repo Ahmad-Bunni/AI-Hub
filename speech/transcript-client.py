eecd204e716b2a27ab79cb7e3b399f7b9ebaf1a8
DESKTOP-3E4KQ2J\Ahmad <Ahmad-Bunni@hotmail.com>
2024-02-28 21:31:08 +0100

    whisper

--- speech/transcript-client.py
+++ speech/transcript-client.py
@@ -3,9 +3,10 @@ from whisper_live.client import TranscriptionClient
 client = TranscriptionClient(
     "localhost",
     9090,
-    is_multilingual=False,
-    translate=True,
-    model_size="tiny",
+    lang="en",
+    translate=False,
+    model="small",
+    use_vad=False,
 )
 
 client()
